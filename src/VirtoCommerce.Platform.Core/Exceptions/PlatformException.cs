6739aa135db0ed4e2adb60f27a05ed05c45b935d
GavriloS <gavrilostanic2@gmail.com>
2021-11-07 18:09:20 +0100

    Fixed implementation of ISerialization

--- src/VirtoCommerce.Platform.Core/Exceptions/PlatformException.cs
+++ src/VirtoCommerce.Platform.Core/Exceptions/PlatformException.cs
@@ -1,12 +1,21 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace VirtoCommerce.Platform.Core.Exceptions
 {
     /// <summary>
     /// Main platform exception type
     /// </summary>
+    ///
+
+    [Serializable]
     public class PlatformException : Exception
     {
+        protected PlatformException(SerializationInfo info, StreamingContext context)
+        {
+
+        }
+
         public PlatformException(string message)
             : base(message)
         {
