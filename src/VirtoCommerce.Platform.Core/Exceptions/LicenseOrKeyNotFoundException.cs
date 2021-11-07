256354c85e8d0ea56a9131cc1d0c53d1398058c2
Vuk Pekez <vukpekez@uns.ac.rs>
2021-11-07 21:18:25 +0100

    Fixed serialization pattern (ISerializable)

--- src/VirtoCommerce.Platform.Core/Exceptions/LicenseOrKeyNotFoundException.cs
+++ src/VirtoCommerce.Platform.Core/Exceptions/LicenseOrKeyNotFoundException.cs
@@ -2,14 +2,14 @@ using System;
 
 namespace VirtoCommerce.Platform.Core.Exceptions
 {
+    [Serializable]
     public class LicenseOrKeyNotFoundException : PlatformException
     {
-        public LicenseOrKeyNotFoundException(string path) : base($"The License or Public Key {path} aren't found.")
+        protected LicenseOrKeyNotFoundException(string path) : base($"The License or Public Key {path} aren't found.")
         {
             
         }
-
-        public LicenseOrKeyNotFoundException(string message, Exception innerException) : base(message, innerException)
+        protected LicenseOrKeyNotFoundException(string message, Exception innerException) : base(message, innerException)
         {
             
         }
