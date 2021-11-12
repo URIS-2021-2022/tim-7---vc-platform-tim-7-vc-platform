15c2ef3c5e4f5fb3ff250f2556b90edb0bc82fc3
MladenBajic <mladen.bajic@servalit.com>
2021-11-12 19:01:49 +0100

    Hardcoded connection string changed csharpsquid:S2068

--- src/VirtoCommerce.Platform.Data/Repositories/DesignTimeDbContextFactory.cs
+++ src/VirtoCommerce.Platform.Data/Repositories/DesignTimeDbContextFactory.cs
@@ -1,15 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 namespace VirtoCommerce.Platform.Data.Repositories
 {
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PlatformDbContext>
     {
+
+        public IConfiguration Configuration { get; }
+
+        public DesignTimeDbContextFactory(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public PlatformDbContext CreateDbContext(string[] args)
         {
             var builder = new DbContextOptionsBuilder<PlatformDbContext>();
 
-            builder.UseSqlServer("Data Source=(local);Initial Catalog=VirtoCommerce3;Persist Security Info=True;User ID=virto;Password=virto;MultipleActiveResultSets=True;Connect Timeout=30");
+            builder.UseSqlServer(Configuration.GetConnectionString("VirtoCommerce"));
 
             return new PlatformDbContext(builder.Options);
         }
