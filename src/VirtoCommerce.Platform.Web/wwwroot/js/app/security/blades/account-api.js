2169007a4b38e65c95ab80c2e1a04889fd2b07cc
MladenBajic <mladen.bajic@servalit.com>
2021-11-12 19:10:40 +0100

    remove Math.random() and generate random number on safe way javascript:S2245

--- src/VirtoCommerce.Platform.Web/wwwroot/js/app/security/blades/account-api.js
+++ src/VirtoCommerce.Platform.Web/wwwroot/js/app/security/blades/account-api.js
@@ -85,9 +85,13 @@ angular.module('platformWebApp')
             }
         ];
 
+        const crypto = window.crypto || window.msCrypto;
+        var array = new Uint32Array(1);
+        crypto.getRandomValues(array);
+
         function generateNewApiKey() {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                var r = array[0] * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             });
         }
